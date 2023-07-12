dd87e98d98e86921c5eae981a89372720c43491b
DESKTOP-4RADET5\admin <jason.wong@sixfacet.com>
2023-07-12 14:38:25 +0800

    add createDate

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 const axios = require('axios'); 
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+const { Client } = require('pg')
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'some2some',
+  password: 'postgres',
+  port: 5432,
+})
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 const csvWriter = createCsvWriter({
   path: 'instagram.csv',
   header: [
     {id: 'id', title: 'No'},
     {id: 'content', title: 'Content'},
-    {id: 'createDate', title: 'Create Date'}
+    {id: 'createDate', title: 'CreateDate'}
   ]
 })
 
 const url = 'https://graph.facebook.com/v17.0/355915284524115/feed?access_token=';
-const access_token = 'EAADg4X8ZAH5QBABjV9yR12e08mUBZAe54CoUUsGBELuHDWotNsYGEnbL2s1XHOFzvxqWwYuhSonubbgwMyAvBszTUxsNBMY7oiye2LI1T0Ss96i6qpDVgNLUPXGfJRRHUNSCxhvPfT5P9fIZBMFWxJXWSLEMWssBHjK7YE0eY4n20Td6G8SJs6ZA7NbBk0wHuMx85tHEZAjQ1yskrcyBU';
+const access_token = 'EAADg4X8ZAH5QBAMdeLJsOOTtSyOBg4mpaxZCJAK2JhVHdx3keIPo0R8am5XGwKnq9jYaXEWWDestJDpnckxw8tl2CsP8D9MJToJQISrMKO0vovUSj5opV2Itf95IYbA0swP4UgZCFVrA17RZClEzkyuNg1hZC1JVTXq3IYn6o3qkZBCLZBEqsKK87fMLZAGKZCogORsJxWMDk8CPGyfcqu5W1';
 var dataArr = [];
+var lastProcessDate;
 
+const query = `SELECT * FROM config where config_name = 'PROCESS_DATE'`;
+
+client.query(query, (err, res) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    for (let row of res.rows) {
+       lastProcessDate = row.date;
+    }
+});
 
 axios.get(url + access_token)
 .then(function(content){
@@ -26,7 +52,7 @@ function processContent(content){
         const id = temp.id;
         const contents = temp.message;
         const createTime = temp.created_time;
-        const currentDate = new Date('2023-01-01');
+        const currentDate = new Date(lastProcessDate);
         const contentDate = new Date(createTime);
         if(contentDate > currentDate){
             dataArr.push({id: id, content: contents, createDate: createTime});
@@ -39,6 +65,7 @@ function processContent(content){
         })
     } else {
         csvWriter.writeRecords(dataArr);
+        console.log('END');
     }
 }
 
